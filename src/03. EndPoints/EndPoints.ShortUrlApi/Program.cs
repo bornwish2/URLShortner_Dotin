93d92c5bf900f088621f10ec6ee607e53aa41812
bornwish2 <65095182+bornwish2@users.noreply.github.com>
2022-11-01 18:05:53 +0330

    feat: complate event sourcing and command handler

--- src/03. EndPoints/EndPoints.ShortUrlApi/Program.cs
+++ src/03. EndPoints/EndPoints.ShortUrlApi/Program.cs
@@ -1,35 +1,117 @@
+using Autofac.Extensions.DependencyInjection;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+using NLog.Web;
+using System.Net;
+
 namespace EndPoints.ShortUrlApi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            #region Sentry/NLog
 
-            // Add services to the container.
+            //We used NLog.Targets.Sentry2 library formerly
+            //But it was not based on NetStandard and used an unstable SharpRaven library
+            //So we decided to replace it with a better library
+            //The NLog.Targets.Sentry3 library supports NetStandard2.0 and uses an updated version of SharpRaven library.
+            //But Sentry.NLog is the official sentry library integrated with nlog and better than all others.
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            //NLog.Targets.Sentry3
+            //https://github.com/CurtisInstruments/NLog.Targets.Sentry
 
-            var app = builder.Build();
+            //Sentry SDK for .NET
+            //https://github.com/getsentry/sentry-dotnet
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            //Sample integration of NLog with Sentry
+            //https://github.com/getsentry/sentry-dotnet/tree/master/samples/Sentry.Samples.NLog
+
+
+            //Set deafult proxy
+            WebRequest.DefaultWebProxy = new WebProxy("http://127.0.0.1:8118", true) { UseDefaultCredentials = true };
+
+            // You can configure your logger using a configuration file:
+
+            // If using an NLog.config xml file, NLog will load the configuration automatically Or, if using a
+            // different file, you can call the following to load it for you: 
+            //LogManager.Configuration = LogManager.LoadConfiguration("NLog-file.config").Configuration;
+
+            var logger = LogManager.GetCurrentClassLogger();
+
+            // Or you can configure it with code:
+            //UsingCodeConfiguration();
+
+            #endregion
+
+            try
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                logger.Debug("init main");
+                await CreateHostBuilder(args).Build().RunAsync();
             }
+            catch (Exception ex)
+            {
+                //NLog: catch setup errors
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                LogManager.Flush();
+                LogManager.Shutdown();
+            }
+        }
 
-            app.UseHttpsRedirection();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureLogging(options => options.ClearProviders())
+                .UseNLog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    //webBuilder.ConfigureLogging(options => options.ClearProviders());
+                    //webBuilder.UseNLog();
+                    webBuilder.UseStartup<Startup>();
+                });
 
-            app.UseAuthorization();
+        private static void UsingCodeConfiguration()
+        {
+            // Other overloads exist, for example, configure the SDK with only the DSN or no parameters at all.
+            var config = new LoggingConfiguration();
 
+            config.AddSentry(options =>
+            {
+                options.Layout = "${message}";
+                options.BreadcrumbLayout = "${logger}: ${message}"; // Optionally specify a separate format for breadcrumbs
 
-            app.MapControllers();
+                options.MinimumBreadcrumbLevel = NLog.LogLevel.Debug; // Debug and higher are stored as breadcrumbs (default is Info)
+                options.MinimumEventLevel = NLog.LogLevel.Error; // Error and higher is sent as event (default is Error)
 
-            app.Run();
+                // If DSN is not set, the SDK will look for an environment variable called SENTRY_DSN. If
+                // nothing is found, SDK is disabled.
+                options.Dsn = "https://a48f67497c814561aca2c66fa5ee37fc:a5af1a051d6f4f09bdd82472d5c2629d@sentry.io/1340240";
+
+                options.AttachStacktrace = true;
+                options.SendDefaultPii = true; // Send Personal Identifiable information like the username of the user logged in to the device
+
+                options.IncludeEventDataOnBreadcrumbs = true; // Optionally include event properties with breadcrumbs
+                options.ShutdownTimeoutSeconds = 5;
+
+                options.AddTag("logger", "${logger}");  // Send the logger name as a tag
+
+                options.HttpProxy = new WebProxy("http://127.0.0.1:8118", true) { UseDefaultCredentials = true };
+                // Other configuration
+            });
+
+            config.AddTarget(new DebuggerTarget("Debugger"));
+            config.AddTarget(new ColoredConsoleTarget("Console"));
+
+            config.AddRuleForAllLevels("Console");
+            config.AddRuleForAllLevels("Debugger");
+
+            LogManager.Configuration = config;
         }
     }
 }
